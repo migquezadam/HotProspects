6c3069146779bc955e5949c163ba2a4cfa9181c4
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-13 01:46:33 -0400

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Miguel Quezada on 13-06-22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
